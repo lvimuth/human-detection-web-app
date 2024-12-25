a5da2c4acc12427644705e06b136091a2ec8918b
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-25 12:44:53 +0400

    theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,63 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
+import Webcam from "react-webcam";
+import { Separator } from "@/components/ui/separator";
+import { ModeToggle } from "@/components/theme-toggle";
+import { FlipHorizontal } from "lucide-react";
 
 type Props = {};
 
 const HomePage = (props: Props) => {
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  //State
+  const [mirrored, setMirrored] = useState<boolean>(false);
+
   return (
-    <div>
-      HomePage
-      <Button variant="outline">Button</Button>
+    <div className="flex h-screen">
+      <div className="relative">
+        <div className="relative h-screen w-full">
+          <Webcam
+            ref={webcamRef}
+            mirrored={mirrored}
+            className="h-full w-full object-contain p-2"
+          />
+          <canvas
+            ref={canvasRef}
+            className="absolute top-0 h-full w-full object-contain"
+          ></canvas>
+        </div>
+      </div>
+
+      {/* Right division - container for button panel and wiki section */}
+      <div className="flex flex-row flex-1">
+        <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
+          {/* Top Section */}
+          <div className="flex flex-col gap-2">
+            <ModeToggle />
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                setMirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal />
+            </Button>
+            <Separator className="my-2" />
+          </div>
+          {/* Middle Section */}
+          <div className="flex flex-col gap-2">
+            <Separator />
+            <Button variant={'outline'} size={'icon'}></Button>
+            <Separator />
+          </div>
+          {/* Bottom Section */}
+          <div className="flex flex-col gap-2"></div>
+        </div>
+      </div>
     </div>
   );
 };
